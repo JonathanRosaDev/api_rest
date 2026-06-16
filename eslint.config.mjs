f67c96ae2aa43a8f83771f279acf64c7b240c4b5
Jonathan Rosa <jonathanrosadev@gmail.com>
2026-06-16 11:22:15 -0300

    modifiquei o model de fotos e fiz a migration da tabela de fotos dos alunos

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig([
       "no-trailing-spaces": "error",
       "eol-last": ["error", "always"],
       "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }], // máximo 1 linha em branco
-      "class-method-use-this": "off"
+      "class-method-use-this": "off",
     },
   },
 ]);
--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,13 +1,10 @@
 import { Router } from 'express';
-import multer from 'multer';
+import loginRequired from '../middlewares/loginRequired.js';
 
 import photoController from '../controllers/PhotoController.js';
-import multerConfig from '../config/multerConfig.js';
-
-const upload = multer(multerConfig);
 
 const router = Router();
 
-router.post('/',upload.single('photo'), photoController.store);
+router.post('/',loginRequired, photoController.store);
 
 export default router;
